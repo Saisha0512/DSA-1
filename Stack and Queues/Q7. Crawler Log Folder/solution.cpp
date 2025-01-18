405e17e248d3aa5e534a50fbf1ed653eff7ebaef
Shubh Krishna <135266175+Shubh-Krishna@users.noreply.github.com>
2025-01-19 00:52:36 +0530

    Update solution.cpp

--- Stack and Queues/Q7. Crawler Log Folder/solution.cpp
+++ Stack and Queues/Q7. Crawler Log Folder/solution.cpp
@@ -1,23 +1,23 @@
-    int minOperations(vector<string>& logs) {
-        stack<string>s;
+int minOperations(vector<string>& logs) {
+    stack<string>s;
 
-        for(auto l : logs){
-            if(l == "../"){
-                if(!s.empty()){
-                    s.pop();
-                }
-            }else if( l == "./"){
-            }else{
-                int len = l.length();
-                if(l[len-1] == '/'){
-                    s.push(l);
-                }
+    for(auto l : logs){
+        if(l == "../"){
+            if(!s.empty()){
+                s.pop();
+            }
+        }else if( l == "./"){
+        }else{
+            int len = l.length();
+            if(l[len-1] == '/'){
+                s.push(l);
             }
         }
+    }
 
-        
-        if(s.empty()){
-            return 0;
-        }else{
-            return s.size();
-        }
+    
+    if(s.empty()){
+        return 0;
+    }else{
+        return s.size();
+    }
